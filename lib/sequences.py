f56aebaf0f068d7700fbcb71c9d937c8c2406b3f
Bailey Cage <bailey.cage@gmail.com>
2023-02-07 12:28:23 -0500

    complete print_fibonacci function

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci = [0, 1]
+    if length == 0:
+        fibonacci.clear()
+    elif length == 1:
+        fibonacci.pop()
+    else:  
+        for n in range(2, length):
+            num = fibonacci[n-2] + fibonacci[n-1]
+            fibonacci.append(num)
+    print(fibonacci)
